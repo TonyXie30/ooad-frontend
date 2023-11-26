bc32f98b91aa92bef4c5f7a04129ae67b3e2a74a
13938 <tonyjohnsonxie30@gmail.com>
2023-11-26 13:41:05 +0800

    增加前端登录拦截器

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      redirect: '/login'
+      redirect: '/home'
     },
     {
       path: '/login',
@@ -18,7 +18,8 @@ const router = createRouter({
     {
       path: '/home',
       name: 'home',
-      component: HelloWorld
+      component: HelloWorld,
+      meta:{requiresAuth:true}
     },
     {
       path: '/register',
@@ -27,7 +28,25 @@ const router = createRouter({
     }
   ],
 });
+router.beforeEach((to, from, next) => {
+  // 检查路由是否需要登录权限
+  if (to.meta.requiresAuth) {
+    // 在这里根据你的实际情况检查用户是否已登录
+    // 例如，你可以检查是否有存储在本地的用户信息
+    const isAuthenticated = localStorage.getItem('username');
 
+    if (isAuthenticated) {
+      // 用户已登录，允许访问
+      next();
+    } else {
+      // 用户未登录，重定向到登录页面
+      next('/login');
+    }
+  } else {
+    // 不需要登录权限的路由，直接放行
+    next();
+  }
+});
 export default router;
 
 
