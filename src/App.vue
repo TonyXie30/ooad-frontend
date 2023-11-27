bd9d7294f352b26be770cac2729ac54a8b0bdef8
Zhiming Zhang <zzm_0323@berkeley.edu>
2023-11-27 23:57:26 +0800

    Conflict fixed

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,11 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view />
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {},
-  mounted() {
-    document.body.style.margin = '0';
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  background-image: url("../public/login-bg.jpg");
-  background-repeat:no-repeat;
-  background-attachment:fixed;
-  background-size: cover;
-  background-position: center center;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  margin: 0;
-}
-</style>
